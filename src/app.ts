33a206f5ccc0df0cb435f81f0ff4bc31fe036779
programarreza <programarreza@gmail.com>
2025-02-01 22:39:17 +0600

    message update from app

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(globalErrorHandler);
 app.use((req: Request, res: Response, next: NextFunction) => {
    res.status(httpStatus.NOT_FOUND).json({
       success: false,
-      message: 'Oops! Looks like you hit the wrong route',
+      message: 'Sorry, the route you are looking for does not exist 🚧',
       error: {
          path: req.originalUrl,
          error: `The requested URL was not found on this server.`,
